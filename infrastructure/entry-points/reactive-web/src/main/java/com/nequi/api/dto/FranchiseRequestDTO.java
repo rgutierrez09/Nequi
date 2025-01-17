eab6632fa7b013c5fae720ab37d019d8ce213fa3
RonaldGG <ronaldgutierrezgarzon10@gmail.com>
2025-01-17 15:44:11 -0500

    refactorizando request

--- infrastructure/entry-points/reactive-web/src/main/java/com/nequi/api/dto/FranchiseRequestDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/com/nequi/api/dto/FranchiseRequestDTO.java
@@ -1,8 +1,6 @@
 package com.nequi.api.dto;
 
-import com.nequi.models.Product;
-
 import java.util.List;
 
-public record FranchiseRequestDTO(String name, List<Product> products ) {
+public record FranchiseRequestDTO(String name, List<Integer> branchIds ) {
 }
